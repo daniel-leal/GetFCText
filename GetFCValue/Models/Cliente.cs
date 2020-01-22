9a21663ec1d1929638746bda5ee0dc685be25c48
Daniel Santos Borges Leal <dleal@banpara.com>
2020-01-22 09:29:01 -0300

    Init reop

---
+++ GetFCValue/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿namespace GetFCValue.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string CpfCnpj { get; set; }
+
+        public int TipoPessoa { get; set; }
+    }
+
+    public enum TipoPessoa
+    {
+        Fisica = 1,
+        Juridica = 2
+    }
+}
